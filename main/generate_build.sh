6fcecf7be5d933c8d3076b8d47f77e81c424650f
Ben Cardoen <bencardoen@gmail.com>
2015-02-27 21:05:34 +0100

    Add tested build code from prototypes.

---
+++ main/generate_build.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+
+###############################################################################
+# Generic build script.
+# Author Ben Cardoen
+# Given sourcecode & CMake , runs CMake with preconfigured options.
+###############################################################################
+
+# Assume pwd = projectdir (in other words, right after a git clone)
+# Source code and CMakeLists in ./main/
+# Generate build folder in ./build/
+
+BUILD_DIR="build"
+
+if [ -d "$BUILD_DIR" ]
+	then
+	if [ -L "$BUILD_DIR" ]
+		then
+		echo "Build directory found, but it's a link. Computer says no."
+	else
+        echo "Found stale build directory : Removing"
+		rm -r $BUILD_DIR
+	fi
+fi
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+
+## Generate Eclipse IDE project files
+# ARG1 argument is not needed for compilation but ensures the indexer in eclipse actually works.
+
+# Uncomment to use clang++ as compiler.
+cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_CXX_COMPILER_ARG1=-std=c++11 -DCMAKE_CXX_COMPILER="clang++" -DCMAKE_BUILD_TYPE=Debug ../main
+
+# G++
+#cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_CXX_COMPILER_ARG1=-std=c++1y -DCMAKE_CXX_COMPILER="g++" -DCMAKE_BUILD_TYPE=Debug ../main
+
+# Compile & link everything in build, assuming quad core
+make all -j8
