8d4d4be80da3cef8025f7cad1a6234a90a728e19
TimTuijn <tim.tuijn@gmail.com>
2015-04-09 15:53:04 +0200

    cosmetic, comments, parent pointer in model, beautiful day, i'm off

--- main/src/model/coupledmodel.cpp
+++ main/src/model/coupledmodel.cpp
@@ -9,35 +9,24 @@
 
 namespace n_model {
 
-/*
- * Constructor for CoupledModel
- *
- * @param name The name of the coupled model
- */
 CoupledModel::CoupledModel(std::string name)
 	: Model(name)
 {
 
 }
 
-/*
- * Adds a submodel to this coupled model
- *
- * @param model The submodel that is to be added
- */
 void CoupledModel::addSubModel(const t_modelptr& model)
 {
 	this->m_components.push_back(model);
 }
 
-/*
- * Connects the given ports with eachother (with a zFunction)
- * The zFunction will not change the message by default
- *
- * @param p1 pointer to output port number
- * @param p2 pointer to input port number
- * @param zFunction the zFunction that has to be executed on the message before transmitting it
- */
+void CoupledModel::resetParents()
+{
+	m_parent.reset();
+	for (auto& child : m_components)
+		child->resetParents();
+}
+
 void CoupledModel::connectPorts(const t_portptr& p1, const t_portptr& p2, t_zfunc zFunction)
 {
 	//TODO make sure that the connection is not illegal!
@@ -46,11 +35,6 @@ void CoupledModel::connectPorts(const t_portptr& p1, const t_portptr& p2, t_zfun
 	p2->setInPort(p1);
 }
 
-/*
- * Get all the components (submodels)
- *
- * @return a vector with pointers to these models
- */
 std::vector<t_modelptr> CoupledModel::getComponents() const
 {
 	return m_components;
