750ea87ca78b4490c0225a4bdcfed5e4b3c6a1f6
Lttlemoi <lttlemoi+git@gmail.com>
2015-04-25 16:43:49 +0200

    * removed tracerID from the tracemessage * write the Tracer part of the report in a separate file so that it doesn't clutter the main file. (the thing is 7 pages long!) - - -> the main file contains a link to this second report - - -> If you want to, I can always include this file in the main report. I just thought that this was cleaner

--- main/src/tracers/tracerbase.h
+++ main/src/tracers/tracerbase.h
@@ -18,25 +18,13 @@ namespace n_tracers {
 class TracerBase
 {
 private:
-	const std::size_t m_id;
-	static std::size_t m_lastid;
 
 protected:
 	TracerBase()
-		: m_id(m_lastid++)
 	{
 	}
 
 public:
-	/**
-	 * @return the ID of this tracer.
-	 * Each tracer has a unique ID that can be used to order
-	 * TracerMessages of different tracers for the same event and stuff like that.
-	 */
-	std::size_t getID() const
-	{
-		return m_id;
-	}
 };
 
 } /* namespace n_tracers */
