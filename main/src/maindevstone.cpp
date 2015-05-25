85fd03477c451f50254904f77f1d258521452c4f
Matthijs Van Os <matthijs3011@gmail.com>
2015-05-25 16:44:51 +0200

    DEVStone and PHOLD executables are configurable through arguments

--- main/src/maindevstone.cpp
+++ main/src/maindevstone.cpp
@@ -7,11 +7,28 @@
 #include "controllerconfig.h"
 #include "coutredirect.h"
 #include "devstone.h"
+#include "stringtools.h"
 
 LOG_INIT("devstone.log")
 
-int main(int, char**)
+/**
+ * The executable takes up to 2 arguments (in this order):
+ * - The width (amount of processors per coupled model)
+ * - The depth (amount of linked coupled models)
+ */
+int main(int argc, char** args)
 {
+	// default values:
+	std::size_t width = 2;
+	std::size_t depth = 3;
+
+	if(argc >= 2) {
+		width = n_tools::toInt(args[1]);
+	}
+	if(argc >= 3) {
+		depth = n_tools::toInt(args[2]);
+	}
+
 	n_control::ControllerConfig conf;
 	conf.name = "DEVStone";
 	conf.saveInterval = 1;
@@ -24,7 +41,7 @@ int main(int, char**)
 		ctrl->setTerminationTime(endTime);
 
 		// Create a DEVStone simulation with width 2 and depth 3
-		t_coupledmodelptr d = n_tools::createObject< n_devstone::DEVStone>(2, 3, false);
+		t_coupledmodelptr d = n_tools::createObject< n_devstone::DEVStone>(width, depth, false);
 		ctrl->addModel(d);
 
 		ctrl->simulate();
--- main/src/mainphold.cpp
+++ main/src/mainphold.cpp
@@ -8,10 +8,38 @@
 #include "coutredirect.h"
 #include "phold.h"
 
-LOG_INIT("devstone.log")
+LOG_INIT("phold.log")
 
-int main(int, char**)
+
+/**
+ * The executable takes up to 4 arguments (in this order):
+ * - the amount of nodes of the PHOLD
+ * - the amount of atomic models per node
+ * - the amount of iterations
+ * - the @b percentage of remotes (example: if you want 42% remotes, enter 42)
+ */
+int main(int argc, char** args)
 {
+	// default values:
+	std::size_t nodes = 1;
+	std::size_t apn = 10;
+	std::size_t iter = 0;
+	float percentageRemotes = 0.1;
+
+	if(argc >= 2) {
+		nodes = n_tools::toInt(args[1]);
+	}
+	if(argc >= 3) {
+		apn = n_tools::toInt(args[2]);
+	}
+	if(argc >= 4) {
+		iter = n_tools::toInt(args[3]);
+	}
+	if(argc >= 5) {
+		percentageRemotes = n_tools::toInt(args[4]) / 100;
+	}
+
+
 	n_control::ControllerConfig conf;
 	conf.name = "DEVStone";
 	conf.saveInterval = 1;
@@ -24,7 +52,7 @@ int main(int, char**)
 		ctrl->setTerminationTime(endTime);
 
 		// Create a DEVStone simulation with width 2 and depth 3
-		t_coupledmodelptr d = n_tools::createObject< n_benchmarks_phold::PHOLD>(1, 10, 0, 0.1);
+		t_coupledmodelptr d = n_tools::createObject< n_benchmarks_phold::PHOLD>(nodes, apn, iter, percentageRemotes);
 		ctrl->addModel(d);
 
 		ctrl->simulate();
